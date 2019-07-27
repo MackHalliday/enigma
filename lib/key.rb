30194daf7696cf0a272af157a0be73a8307780c8
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-26 21:10:53 -0600

    Refactoring with only Key attribute as five digit num

--- lib/key.rb
+++ lib/key.rb
@@ -2,31 +2,30 @@ require 'pry'
 
 class Key
 
-  attr_accessor :keys, :five_digit
+  attr_accessor :key
 
-  def initialize(five_digit = random_num)
-    @five_digit = five_digit
-    #@keys = keys
+  def initialize(key = random_num)
+    @key = key
   end
+
 
   def random_num
     random_digits = []
     5.times do random_digits << rand(0..9).to_s
     end
-    @five_digit = random_digits.join
-  end
-  #
-  def create_num_pairs
-    @five_digit.split(//).each_cons(2).to_a
+    @key = random_digits.join
   end
 
-  def join_pairs_nums_together
+  def create_pairs
+    @key.split(//).each_cons(2).to_a
+  end
+
+  def join_pairs_together
     create_num_pairs.map {|pair| pair[0].to_s + pair[1].to_s}
   end
 
-  def set_num_values_as_keys
+  def created_key_values
     keys = [:a, :b, :c, :d]
     key_hash = Hash[keys.zip join_pairs_nums_together]
-    @keys = key_hash
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -7,23 +7,23 @@ require 'mocha/minitest'
 class KeyTest < MiniTest::Test
 
   def setup
-    #five digit randomly given
-    @key_1 = Key.new
-    @key_1.stubs(:five_digit).returns("01221")
 
-    #five digit provided
-    @key_2 = Key.new("00234")
+     @key_given = Key.new("00234")
+
+     #@key_not_given = Key.new
+     #@key_not_give.stubs(:random_number).returns("01221")
+
   end
 
   def test_it_exist
 
-    assert_instance_of Key, @key_1
-    #assert_instance_of Key, @key_2
+    assert_instance_of Key, @key_given
+    #assert_instance_of Key, @key_not_given
   end
 
   def test_attributes_of_key
-    #Testing attribute or testing stub?
 
-    assert_equal "01221", @key_1.five_digit
-    #assert_equal "00234", @key_2.five_digit
+    assert_equal "00234", @key_given.key
+    #assert_equal "01221", @key_not_given.key
   end
+end
