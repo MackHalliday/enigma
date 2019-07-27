c8ee08500769e936a876a388eb57b41a466cdfe1
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-26 20:06:41 -0600

    Create methods to create key values

--- lib/key.rb
+++ lib/key.rb
@@ -1,4 +1,34 @@
-binding.pry
+require 'pry'
 
 class Key
-end 
+
+  attr_reader :keys
+
+  def initialize()
+    @keys = {}
+  end
+
+  def random_num
+    random_digits = []
+    5.times do random_digits << rand(0..9)
+    end
+    random_digits
+  end
+
+  def create_num_pairs
+    random_num.each_cons(2).to_a
+  end
+
+  def join_pairs_nums_together
+    create_num_pairs.map {|pair| pair[0].to_s + pair[1].to_s}
+  end
+
+  def set_num_values_as_keys
+    keys = [:a, :b, :c, :d]
+    key_hash = Hash[keys.zip join_pairs_nums_together]
+  end
+
+  def set_key_values
+    @keys = set_num_values_as_keys
+  end
+end
--- test/key_test.rb
+++ test/key_test.rb
@@ -13,4 +13,37 @@ class KeyTest < MiniTest::Test
 
     assert_instance_of Key, @key_1
   end
+
+  def test_attributes_of_key
+
+    assert_equal ({}), @key_1.keys
+  end
+
+  def test_random_five_digit_number
+
+    assert_equal "10000", @key_1.random_num
+  end
+
+  def test_create_pairs
+
+    assert_equal [10, 04, 40, 02], @key_1.create_num_pairs
+  end
+
+  def test_pairs_can_be_joined
+
+    assert_equal [10, 04, 40, 02], @key_1.join_pairs_nums_together
+  end
+
+  def test_set_values_as_keys
+
+    assert_equal 100, @key_1.set_num_values_as_keys
+  end
+
+  def test_can_update_key_attribute
+    @key_1.set_key_values
+
+    assert_equal 1000, @key_1.keys
+  end
+
+
 end
