76eaeb3eabd47a358e7c7bc34cbed26fc5c2bf0f
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-27 21:36:01 -0600

    Cleaned up code

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,8 +13,10 @@ class Enigma
 
     offset_1 = Offset.new(date)
     offset_hash = key.get_key_values
+
     shift = Shift.new(message, key_hash, offset_hash)
 
+    #shift.decode_message
   end
 
 end
