1f0116fa1654d60bcd9f8b4ceb24b8fe3d55b5a4
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-28 21:04:36 -0600

    Assign key letters behavioring as expected

--- lib/shift.rb
+++ lib/shift.rb
@@ -9,6 +9,7 @@ class Shift
     @key_values = key_values
     @offset_values = offset_values
     @characters = ('a'..'z').to_a << ' '
+    @key_categories = [:a, :b, :c, :d]
   end
 
   def create_final_key
@@ -21,25 +22,24 @@ class Shift
   end
 
   def return_key(num)
-    
+
 
   end
 
   def assign_key_letters
-    new_hash = Hash.new{|h,k| h[k] = [] }
+    new_hash = Hash.new{|hash, key| hash[key] = [] }
 
     message.each.with_index do |char, index|
-      new_hash[index % 4] << char
+      new_hash[@key_categories[index % 4]] << char
     end
     new_hash
   end
 
 
   def map_new_letter_values
-    key_shift_letters = [:a, :b, :c, :d]
     key_shift_values = Hash.new
 
-    key_shift_letters.each do |key|
+    @key_categories.each do |key|
     key_shift_values[key] = Hash[@characters.zip(@characters.rotate(create_final_key[key].to_i))]
     end
     key_shift_values
