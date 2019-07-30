c92e14e095542ba4eb61dbaed665d0a6a1c0f90d
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-30 15:21:51 -0600

    Removed random_num, rewrote test accordingly

--- lib/key.rb
+++ lib/key.rb
@@ -9,17 +9,7 @@ class Key
     @key_categories = [:a, :b, :c, :d]
   end
 
-  # def random_num
-  #   random_digits = []
-  #     5.times do random_digits << rand(0..9).to_s
-  #   end
-  #   key = random_digits.join
-  # end
-
   def create_pairs
-    # if @key = 1
-    #   random_num
-    # end
     @key.split(//).each_cons(2).to_a
   end
 
@@ -28,6 +18,6 @@ class Key
   end
 
   def get_key_values
-    key_hash = Hash[@key_categories.zip join_pairs]
+    Hash[@key_categories.zip join_pairs]
   end
 end
