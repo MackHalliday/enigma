aa23fd71fb9fc376bc42c3c374334e830afc9450
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-30 18:38:16 -0600

    Pseudo code for encrypt and decrypt files

---
+++ decrypt.rb
@@ -0,0 +1,7 @@
+#should take 4 args
+#1. file that contains encrypted message
+#2. file to write decrypted message
+#3. key used for decryption
+#4. date used for decryption
+#5. $ ruby ./lib/encrypt.rb message.txt encrypted.txt
+#Created 'encrypted.txt' with the key 82648 and date 240818
---
+++ encrypt.rb
@@ -0,0 +1,4 @@
+#takes two args
+#1. file that is message to encrypt
+#2. file where encrypt message writen
+#should also output key and date
