d3c859134d2ed82e3c021e28f92452c2e4ee3bed
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-30 21:21:33 -0600

    Encrypt file working

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,17 +5,17 @@ require './lib/shift'
 
 enigma = Enigma.new
 
-
-
 handle = File.open(ARGV[0], "r")
+#reads "hello world"
 message = handle.read
 handle.close
 
+#{:encryption=>"w nicvylfgf\n", :key=>"36777", :date=>"300719"}
 encryption = enigma.encrypt(message)
 
 encrypted = File.open(ARGV[1], "w")
+#writes w nicvylfgf
 encrypted.write(encryption[:encryption])
 encrypted.close
 
-
-puts "#{enigma.encrypt(message)}"
+puts "Created 'encrypted.txt' with the key #{encryption[:key]}and date #{encryption[:date]}"
